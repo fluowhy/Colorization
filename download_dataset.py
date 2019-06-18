f37f9843bfe0e5dd813715531bbafc38d949e540
Mauricio <mau.altgr@hotmail.com>
2019-06-18 13:29:49 -0400

    add voc and stl10

--- download_dataset.py
+++ download_dataset.py
@@ -3,5 +3,9 @@ import torchvision
 from torchvision import datasets
 
 
-trainset = torchvision.datasets.ImageNet(root="../datasets/imagenet/train", train=True, download=True)
-testset = torchvision.datasets.ImageNet(root="../datasets/imagenet/test", train=False, download=True)
+trainset = torchvision.datasets.VOCDetection(root="../datasets/vocdet/train", image_set="train", download=True, year="2012")
+testset = torchvision.datasets.VOCDetection(root="../datasets/vocdet/test", image_set="val", download=True, year="2012")
+
+trainset = torchvision.datasets.STL10(root="../datasets/stl10/train", split="train", download=True)
+testset = torchvision.datasets.STL10(root="../datasets/stl10/test", split="test", download=True)
+unlabeled = torchvision.datasets.STL10(root="../datasets/stl10/unlabeled", split="unlabeled", download=True)
