1ae8ea78b3a0dccff267dc0546513054e570021f
mauricio <mau.altgr@hotmail.com>
2019-06-19 18:51:43 -0400

    add cifar100

--- download_dataset.py
+++ download_dataset.py
@@ -3,12 +3,15 @@ import torchvision
 from torchvision import datasets
 
 
-trainset = torchvision.datasets.VOCDetection(root="../datasets/vocdet/train", image_set="train", download=True, year="2012")
-testset = torchvision.datasets.VOCDetection(root="../datasets/vocdet/test", image_set="val", download=True, year="2012")
+torchvision.datasets.VOCDetection(root="../datasets/vocdet/train", image_set="train", download=True, year="2012")
+torchvision.datasets.VOCDetection(root="../datasets/vocdet/test", image_set="val", download=True, year="2012")
 
-trainset = torchvision.datasets.STL10(root="../datasets/stl10/train", split="train", download=True)
-testset = torchvision.datasets.STL10(root="../datasets/stl10/test", split="test", download=True)
-unlabeled = torchvision.datasets.STL10(root="../datasets/stl10/unlabeled", split="unlabeled", download=True)
+torchvision.datasets.STL10(root="../datasets/stl10/train", split="train", download=True)
+torchvision.datasets.STL10(root="../datasets/stl10/test", split="test", download=True)
+torchvision.datasets.STL10(root="../datasets/stl10/unlabeled", split="unlabeled", download=True)
 
-trainset = torchvision.datasets.CIFAR10(root="../datasets/cifar10/train", train=True, download=True)
-testset = torchvision.datasets.CIFAR10(root="../datasets/cifar10/test", train=False, download=True)
+torchvision.datasets.CIFAR10(root="../datasets/cifar10/train", train=True, download=True)
+torchvision.datasets.CIFAR10(root="../datasets/cifar10/test", train=False, download=True)
+
+torchvision.datasets.CIFAR100(root="../datasets/cifar100/train", train=True, download=True)
+torchvision.datasets.CIFAR100(root="../datasets/cifar100/train", train=False, download=True)
