ed200b4efd2ce30ab616634099bd68cd4b642934
mauricio <mau.altgr@hotmail.com>
2019-07-12 15:32:16 -0400

    add layer to decoder

--- decoder.py
+++ decoder.py
@@ -9,10 +9,13 @@ class DEC(torch.nn.Module):
 
         self.conv1 = torch.nn.Conv2d(in_ch, nf, ks, padding=int((ks - 1)*0.5))
         self.bn1 = torch.nn.BatchNorm2d(nf)
-        self.conv2 = torch.nn.Conv2d(nf, out_ch, ks, padding=int((ks - 1)*0.5))
+        self.conv2 = torch.nn.Conv2d(nf, nf, ks, padding=int((ks - 1)*0.5))
+        self.bn2 = torch.nn.BatchNorm2d(nf)
+        self.conv3 = torch.nn.Conv2d(nf, out_ch, ks, padding=int((ks - 1)*0.5))
 
 
     def forward(self, x):
         h = self.relu(self.bn1(self.conv1(x)))
-        out = self.tanh(self.conv2(h))
+        h = self.relu(self.bn2(self.conv2(x)))
+        out = self.tanh(self.conv3(h))
         return out
