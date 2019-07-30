2f476cc06bf2e524ff1b0b7dde9d5fd80ae494f8
Mauricio <mau.altgr@hotmail.com>
2019-07-29 21:01:24 -0400

    fix grey image bug

--- rgb2grey.py
+++ rgb2grey.py
@@ -9,7 +9,7 @@ def rgb2grey(split):
     rgb_images = np.transpose(rgb_images, (0, 2, 3, 1))
     grey_images = np.zeros((n, h, w), dtype=np.uint8)
     for i in tqdm(range(len(rgb_images))):
-        grey = skimage.color.rgb2grey(rgb_images[i])
+        grey = skimage.color.rgb2grey(rgb_images[i]) * 255
         grey_images[i] = grey
     np.save("../datasets/stl10/{}_grey_64.npy".format(split), grey_images)
     return
