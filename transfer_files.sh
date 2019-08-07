e187af7949d77aaa066585b3aaff723f6953dc79
mau <mau.altgr@hotmail.com>
2019-08-07 16:31:39 -0400

    add transfer

--- transfer_files.sh
+++ transfer_files.sh
@@ -1,1 +1,4 @@
 #!/usr/bin/env bash
+
+scp -r mromero@172.17.28.76:/home/mromero/Colorizaion/files /home/mauricio/Documents/seminario/Colorization
+scp -r mromero@172.17.28.76:/home/mromero/Colorizaion/models /home/mauricio/Documents/seminario/Colorization
